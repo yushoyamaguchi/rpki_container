b14985465d5ae14903bbe7ea5fd7b08328c080d9
yushoyamaguchi <ysh.824@outlook.jp>
2022-11-15 13:44:09 +0900

    krill and nginx

--- my_krill1/docker_run_test1.sh
+++ my_krill1/docker_run_test1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name krill_con --privileged -dit yama_krill:test1 /bin/bash
+docker run --name krill_con --privileged -p 8080:80 -dit yama_krill:test1 /bin/bash
--- my_krill1/krill_start1.sh
+++ my_krill1/krill_start1.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 systemctl enable krill
+systemctl enable nginx
 
-#rewrite executer of /usr/lib/systemd/system/routinator.service 
+#rewrite executer of /usr/lib/systemd/system/krill.service 
 sed -i -e 's/User=krill/User=root/g' /usr/lib/systemd/system/krill.service
 
 
+#rewrite executer of nginx
+sed -i -e 's/user www-data;/user root;/g' /etc/nginx/nginx.conf
+
+
 systemctl start krill
+
+
+rm /etc/nginx/sites-enabled/default
+
+cat <<EOL >> /etc/nginx/sites-enabled/krill.example.org
+server {
+      server_name krill.example.org;
+      client_max_body_size 100M;
+
+      location / {
+              proxy_pass https://localhost:3000/;
+      }
+
+  listen 80;
+}
+EOL
+
+systemctl start nginx
