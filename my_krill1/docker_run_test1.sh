a99a993c1d039799a0dbba54e81b773289288b36
yushoyamaguchi <ysh.824@outlook.jp>
2022-11-21 12:14:10 +0900

    krill conf 1

--- my_krill1/docker_run_test1.sh
+++ my_krill1/docker_run_test1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name krill_con --privileged -p 8080:80 -dit yama_krill:test1 /bin/bash
+docker run --name krill_con --privileged -p 8080:80  -p 8030:3000 -dit yama_krill:test1 /bin/bash
