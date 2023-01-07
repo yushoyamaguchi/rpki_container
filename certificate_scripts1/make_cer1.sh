b9484fbc574baa1ddf4bcbde67325f6f35b7d882
yushoyamaguchi <ysh.824@outlook.jp>
2023-01-07 13:00:18 +0900

    証明書生成スクリプト

---
+++ certificate_scripts1/make_cer1.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -
+
+OPENSSL=openssl
+
+DATE=`/bin/date +%Y-%m%d`
+SERIAL=`cat /proc/meminfo | ${OPENSSL} sha1 -sha256 |cut -d " " -f 2 | cut -c 1-11`
+echo $SERIAL
+
+OPENSSLCNF=opensslcnf-template.txt
+CANAME=CANAME
+
+[ -f ${CANAME}.cer ] && mv ${CANAME}.cer ${CANAME}.cer.old
+
+${OPENSSL} req                  \
+        -new                    \
+        -x509                   \
+        -config ${OPENSSLCNF}   \
+        -out  ${CANAME}.cer    \
+        -keyout  ${CANAME}.key  \
+        -outform    DER         \
+        -days       1825     \
+        -set_serial 0x${SERIAL} \
+        -extensions x509v3_extensions
+
+if [ -f ${CANAME}.cer ] ; then
+  echo "converting .cer to .pem."
+  ${OPENSSL} x509 -inform der -in ${CANAME}.cer -outform pem -out ${CANAME}.pem
+  echo "verifing "
+  ${OPENSSL} verify -CAfile ${CANAME}.pem ${CANAME}.pem
+  echo ${CANAME}.cer was created.
+else
+  echo ${CANAME}.cer was not created.
+fi
