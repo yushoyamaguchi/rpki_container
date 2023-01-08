4d7a197e50e1f7b3853c1364ff5923a10f18ec71
yushoyamaguchi <ysh.824@outlook.jp>
2023-01-09 02:09:30 +0900

    routinator tal

--- my_routinator1/routinator_start1.sh
+++ my_routinator1/routinator_start1.sh
@@ -6,6 +6,8 @@ systemctl enable routinator
 #rewrite executer of /usr/lib/systemd/system/routinator.service 
 sed -i -e 's/User=routinator/User=root/g' /usr/lib/systemd/system/routinator.service
 
+mkdir /tal
+routinator --config /etc/routinator/routinator.conf--extra-tals-dir=tal config
 
 systemctl start routinator
 
