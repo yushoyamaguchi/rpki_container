4ea8a641701ce9738b668d7ce67694ab6225de25
yushoyamaguchi <ysh.824@outlook.jp>
2023-04-30 20:49:26 +0900

    routinator and frr

--- full_rsync/krill/krill_regi1.sh
+++ full_rsync/krill/krill_regi1.sh
@@ -231,5 +231,9 @@ echo "export KRILL_CLI_TOKEN=yama80" >>/root/.bashrc
 echo "export KRILL_CLI_MY_CA=ta"  >>/root/.bashrc
 echo "export KRILL_CLI_SERVER=https://localhost:3000/" >>/root/.bashrc
 
+
+export KRILL_CLI_TOKEN=yama80
+export KRILL_CLI_MY_CA=ta
+export KRILL_CLI_SERVER=https://localhost:3000/
 krillc roas update --add "192.168.1.0/24 => 65001"
 krillc roas update --add "192.168.2.0/24 => 65002"
