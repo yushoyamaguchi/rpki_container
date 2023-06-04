82e0d213bde0e8f4937c53e1b289352b2d4bd9ca
yushoyamaguchi <ysh.824@outlook.jp>
2023-06-04 15:48:51 +0900

    [WIP] let's encrypt

--- lets_nginx/Memo.md
+++ lets_nginx/Memo.md
@@ -1,8 +1,19 @@
 # 0603
+## access
+```
+curl localhost:8080
+curl --insecure https://localhost:44300
+```
+
 ## commands
 ```
 certbot --nginx -d krill.example.org --register-unsafely-without-email --agree-tos
+certbot --nginx -d krill.yamaguchi.com --register-unsafely-without-email --agree-tos
 ```
 
 エラーが出た。
 nginxの設定ファイルとの兼ね合いをもう少し調べる。
+
+## 懸念
+Let's Encryptはローカル環境では使えないかも...
+自己署名証明書を使うしかないかもしれない。
--- lets_nginx/nginx_lets1.sh
+++ lets_nginx/nginx_lets1.sh
@@ -10,7 +10,7 @@ sed -i -e 's/user www-data;/user root;/g' /etc/nginx/nginx.conf
 
 rm /etc/nginx/sites-enabled/default
 
-cat <<EOL >> /etc/nginx/sites-enabled/yamaguchi.test
+cat <<EOL >> /etc/nginx/sites-enabled/yamaguchi.conf
 
 server {
     listen 80 ;
@@ -21,18 +21,6 @@ server {
     }
 }
 
-server {
-    listen 443 ;
-    ssl on;
-	server_name www.example.com;
-	ssl_certificate /etc/nginx/ssl/server.crt; # サーバー証明書のパス
-	ssl_certificate_key /etc/nginx/ssl/server.key; # 秘密鍵のパス
-    location / {
-        root /var/www;
-        access_log /var/log/nginx/access.log;
-        error_log /var/log/nginx/error.log;
-    }
-}
 EOL
 
 
@@ -48,16 +36,6 @@ cat <<EOL >>/var/www/index.html
   </body>
 </html>
 EOL
-
-
-
-SJ="/CN=yamaguchi.test"
-mkdir -p /etc/nginx/ssl
-openssl genrsa -out /etc/nginx/ssl/server.key 2048
-openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "$SJ"
-openssl x509 -in /etc/nginx/ssl/server.csr -days 365 -req -signkey /etc/nginx/ssl/server.key > /etc/nginx/ssl/server.crt
-
-
 
 systemctl start nginx
 
