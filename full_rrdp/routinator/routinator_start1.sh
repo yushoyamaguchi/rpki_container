06d755285bd6daed5a84e87d4c37a3144ae97799
yushoyamaguchi <ysh.824@outlook.jp>
2023-06-08 12:38:22 +0900

    [important] full_rrdp

--- full_rrdp/routinator/routinator_start1.sh
+++ full_rrdp/routinator/routinator_start1.sh
@@ -19,3 +19,7 @@ systemctl start rsyslog
 echo -e "10.0.0.1\tkrill.example.org" >> /etc/hosts
 
 curl --insecure http://krill.example.org/ta/ta.tal >> /tal/ta.tal
+
+curl --insecure http://krill.example.org/ta/cert.pem >> cert.pem
+
+curl --insecure http://krill.example.org/ta/rootCA.crt >> rootCA.crt
