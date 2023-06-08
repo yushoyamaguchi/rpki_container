f9dbea2bb19f407d282d62ff641e579ca37ffed5
yushoyamaguchi <ysh.824@outlook.jp>
2023-06-08 23:28:47 +0900

    can not exec source command in docker build

---
+++ routinator_build/rm_container.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+
+docker stop CACHE
+docker rm CACHE
---
+++ routinator_build/routinator_start1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+systemctl enable routinator
+
+#rewrite executer of /usr/lib/systemd/system/routinator.service 
+sed -i -e 's/User=routinator/User=root/g' /usr/lib/systemd/system/routinator.service
+
+mkdir /tal
+routinator --config /etc/routinator/routinator.conf --extra-tals-dir=tal config
+echo "extra-tals-dir = \"/tal\" " >> /etc/routinator/routinator.conf
+
+systemctl start rsyslog
+
+#systemctl start routinator
+
+echo -e "10.0.0.1\tkrill.example.org" >> /etc/hosts
+
+curl --insecure http://krill.example.org/ta/ta.tal >> /tal/ta.tal
+
+curl --insecure http://krill.example.org/ta/ta.cer >> /ta.cer
+
+curl --insecure http://krill.example.org/ta/cert.pem >> cert.pem
+
+curl --insecure http://krill.example.org/ta/rootCA.crt >> rootCA.crt
