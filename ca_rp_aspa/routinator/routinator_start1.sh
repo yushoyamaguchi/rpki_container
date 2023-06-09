28ba86874f1bacb9bcbc6273765e84ca8c189e3b
yushoyamaguchi <ysh.824@outlook.jp>
2023-06-09 16:20:42 +0900

    [important]ca_rp_aspa fin

--- ca_rp_aspa/routinator/routinator_start1.sh
+++ ca_rp_aspa/routinator/routinator_start1.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 
-systemctl enable routinator
-
-#rewrite executer of /usr/lib/systemd/system/routinator.service 
-sed -i -e 's/User=routinator/User=root/g' /usr/lib/systemd/system/routinator.service
-
 mkdir /tal
-routinator --config /etc/routinator/routinator.conf --extra-tals-dir=tal config
-echo "extra-tals-dir = \"/tal\" " >> /etc/routinator/routinator.conf
+
 
 systemctl start rsyslog
 
