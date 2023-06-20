aaf231669ff9c7cb1c877e52e99c07f7ccc7d45a
yushoyamaguchi <ysh.824@outlook.jp>
2023-06-21 02:33:43 +0900

    gen_yaml.py

--- tinet_scale_test/gen_yaml.py
+++ tinet_scale_test/gen_yaml.py
@@ -1,35 +1,42 @@
 import sys
 import os
 
+def minus_one_ifnot_natural(i,nodes):
+    if i == 0:
+        return i-1+nodes
+    else:
+        return i - 1
+
+
 def generate_yaml(nodes):
     data = "nodes:\n"
-    for i in range(1, nodes + 1):
+    for i in range(0, nodes ):
         data += f"""
 - name: R{i}
   image: frrouting/frr:latest
   docker_run_extra_args: --entrypoint bash
   interfaces:
-  - {{ name: net0, type: direct, args: R{(i % nodes) + 1}#net1 }}
-  - {{ name: net1, type: direct, args: R{((i + 1) % nodes) + 1}#net0 }}
+  - {{ name: net0, type: direct, args: R{(i + 1)%nodes}#net1 }}
+  - {{ name: net1, type: direct, args: R{minus_one_ifnot_natural(i,nodes) }#net0 }}
   sysctls:
   - sysctl: net.ipv4.ip_forward=1
 """
     data += "\nnode_configs:\n"
-    for i in range(1, nodes + 1):
+    for i in range(0, nodes ):
         data += f"""
 - name: R{i}
   cmds:
   - cmd: sed -i -e 's/bgpd=no/bgpd=yes/g' /etc/frr/daemons
   - cmd: /usr/lib/frr/frrinit.sh start
   - cmd: ip addr add 10.0.{i}.1/24 dev net0
-  - cmd: ip addr add 10.0.{((i + 1) % nodes) + 1}.2/24 dev net1
+  - cmd: ip addr add 10.0.{minus_one_ifnot_natural(i,nodes) }.2/24 dev net1
   - cmd: >-
       vtysh -c "conf t"
-      -c "router bgp {i}"
+      -c "router bgp {65000+i}"
       -c "no bgp ebgp-requires-policy"
       -c ' bgp router-id 1.1.1.{i}'
-      -c "neighbor 10.0.{i}.2 remote-as {(i % nodes) + 1}"
-      -c "neighbor 10.0.{((i + 1) % nodes) + 1}.1 remote-as {((i + 1) % nodes) + 1}"
+      -c "neighbor 10.0.{i}.2 remote-as {65000+((i+1) % nodes) }"
+      -c "neighbor 10.0.{minus_one_ifnot_natural(i,nodes) }.1 remote-as {65000+minus_one_ifnot_natural(i,nodes) }"
       -c ' address-family ipv4 unicast'
       -c '  redistribute connected'
       -c ' exit-address-family'
